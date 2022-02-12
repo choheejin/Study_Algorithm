db579ca16b9f6c5fd137357bea502bdf7461d64b
choheejin <hjcho9510@gmail.com>
2022-02-12 23:32:37 +0900

    :seedling: XORXORXOR 시간초과 해결 (#3)

--- baekjoon/12833.py
+++ baekjoon/12833.py
@@ -1,6 +1,10 @@
 A, B, C = map(int, input().split())
 
-for i in range(C):
+for i in range(C%2):
     A ^= B
 
 print(A)
+
+# XOR 연산은 두 피연산자의 bit 가 다르면 1, 같으면 0
+# n번째 결과는 n+2번째 결과와 같음
+# 01 ^ 11 => 10 ^ 11 => 01 ^ 11 ...
