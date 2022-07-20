e5a01bedd97dba4306cd2ee1bac3d6ad15e42bfe
Heejin Cho <74577803+choheejin@users.noreply.github.com>
2022-07-20 11:47:07 +0900

    [백준] 11650: 좌표 정렬하기 시간 초과 문제 해결

--- baekjoon/11650.py
+++ baekjoon/11650.py
@@ -3,26 +3,13 @@ import sys
 N = int(sys.stdin.readline())
 
 array = []
-array2 = []
-result = []
-x_zip =[]
+
 
 for _ in range(0, N, 1):
-  array.append(map(int, sys.stdin.readline().split()))
+  a, b = map(int, sys.stdin.readline().split())
+  array.append([a, b])
+
+array.sort()
 
 for x, y in array:
-  x_zip.append(x)
-  array2.append([x, y])
-
-x_zip = set(x_zip)
-
-for i in x_zip:
-  y_zip = []
-  for x, y in array2:
-    if (i == x):
-      y_zip.append(y)
-    
-  y_zip.sort()
-  for y in y_zip:
-    print(i, y)
-
+  print(x, y)
